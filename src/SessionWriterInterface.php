26828524f0153801845e06a8daf3933ba5e726f5
Benoît Ngô <ngobenoit@gmail.com>
2016-12-29 11:33:01 +0100

    Updating comment

--- src/SessionWriterInterface.php
+++ src/SessionWriterInterface.php
@@ -4,7 +4,8 @@ namespace Interop\Session\Writer;
 
 interface SessionWriterInterface {
   /***
-  ** This function should save $toSave into the $_SESSION. IT must ensure the saving, that mean the session should be writed via this function
+  ** This function should save $toSave into the $_SESSION without removing value not contained into $toSave. 
+  * IT must ensure the saving, that mean the session should be writed via this function
   * This function must let the session at the previous state
   **/
   public function save(iterable $toSave): void;
